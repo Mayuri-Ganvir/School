20234ff14ae14b5221a734f56bb4ec51c9ab1188
Mayuri Ganvir <mayuri.kishul@gmail.com>
2022-06-09 17:14:35 +0530

    adding new file aboutpage.dart

---
+++ aboutpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(Material(
+      // ignore: prefer_const_constructors
+      color: Color.fromARGB(221, 140, 107, 224),
+      // ignore: prefer_const_constructors
+      child: Center(
+        // ignore: prefer_const_constructors
+        child: Text(
+          "my about page",
+          textDirection: TextDirection.ltr,
+          // ignore: prefer_const_constructors
+          style: TextStyle(color: Color.fromARGB(255, 25, 211, 171), fontSize: 30.0),
+        ),
+      )));
+}
