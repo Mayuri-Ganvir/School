05b710e30d63744d53f1ee8fe4d3442abac4965b
Mayuri Ganvir <mayuri.kishul@gmail.com>
2022-06-09 16:24:18 +0530

    adding new file

---
+++ mainpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(Material(
+      // ignore: prefer_const_constructors
+      color: Color.fromARGB(221, 232, 229, 36),
+      // ignore: prefer_const_constructors
+      child: Center(
+        // ignore: prefer_const_constructors
+        child: Text(
+          "new modern school",
+          textDirection: TextDirection.ltr,
+          // ignore: prefer_const_constructors
+          style: TextStyle(color: Color.fromARGB(255, 27, 221, 179), fontSize: 30.0),
+        ),
+      )));
+}
